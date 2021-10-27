7e5dc1fd0f70738b502c53001156a9f842512a20
Ruben Preschel <ruben.preschel@valtech.com>
2021-10-27 12:47:40 +0200

    Fixed viewport fail

--- cypress/integration/2-advanced-examples/cypress_api.spec.js
+++ cypress/integration/2-advanced-examples/cypress_api.spec.js
@@ -97,7 +97,7 @@ context('Cypress.config()', () => {
     expect(myConfig).to.have.property('requestTimeout', 5000)
     expect(myConfig).to.have.property('responseTimeout', 30000)
     expect(myConfig).to.have.property('viewportHeight', 900)
-    expect(myConfig).to.have.property('viewportWidth', 1000)
+    expect(myConfig).to.have.property('viewportWidth', 1200)
     expect(myConfig).to.have.property('pageLoadTimeout', 60000)
     expect(myConfig).to.have.property('waitForAnimations', true)
 
